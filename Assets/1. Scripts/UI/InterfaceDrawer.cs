219e0f0fe38240926a6ff21ddc68197df6cf8add
adunyt <65284833+adunyt@users.noreply.github.com>
2023-10-01 16:51:59 +0300

    fix: fix UI updating
    chatgpt is great

--- Assets/1. Scripts/UI/InterfaceDrawer.cs
+++ Assets/1. Scripts/UI/InterfaceDrawer.cs
@@ -6,27 +6,53 @@ using UnityEngine.UI;
 public class InterfaceDrawer : MonoBehaviour
 {
     [SerializeField] private Inventory inventory;
-    private List<ItemDrawer> itemDrawers = new();
+    [SerializeField] private ItemDrawer itemDrawerPrefab; // Reference to your ItemDrawer prefab
+    private List<ItemDrawer> itemDrawers = new List<ItemDrawer>();
 
     private void Awake()
     {
-        itemDrawers.Clear();
-        itemDrawers = new(GetComponentsInChildren<ItemDrawer>());
+        // Initialize the UI based on the current inventory
         UpdateUI();
     }
 
     public void UpdateUI()
     {
+        // Get the current items and counts from the inventory
         var (items, itemsCount) = inventory.GetAllItems();
-        if (itemDrawers.Count < items.Count)
+
+        for (int i = 0; i < items.Count; i++)
         {
-            Debug.LogWarning("More items then ItemDrawers! Skipping overflowing items");
-        }
-        for (int i = 0; i < itemDrawers.Count && i < items.Count; i++)
-        {
-            var itemDrawer = itemDrawers[i];
+            ItemDrawer itemDrawer;
+            if (i < itemDrawers.Count)
+            {
+                // Item drawer already exists; enable it
+                itemDrawer = itemDrawers[i];
+                itemDrawer.gameObject.SetActive(true);
+            }
+            else
+            {
+                // Item drawer doesn't exist; instantiate and add it
+                itemDrawer = InstantiateItemDrawer(i);
+                itemDrawers.Add(itemDrawer);
+            }
+
+            // Set the item and count in the item drawer
             itemDrawer.SetItem(items[i], itemsCount[i]);
         }
 
+        // Disable any extra item drawers that are not needed
+        for (int i = items.Count; i < itemDrawers.Count; i++)
+        {
+            itemDrawers[i].gameObject.SetActive(false);
+        }
     }
+
+    private ItemDrawer InstantiateItemDrawer(int index)
+    {
+        // Instantiate a new item drawer and set its parent to the current panel
+        ItemDrawer itemDrawer = Instantiate(itemDrawerPrefab, transform);
+        itemDrawer.name = $"ItemDrawer_{index}"; // Optional: Rename for clarity
+        return itemDrawer;
+    }
+
 }
